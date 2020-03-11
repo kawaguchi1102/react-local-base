fe7f88f6097d6e18ff358be853604a36d8e940e8
kawaguchi <info@kawaguchi-design.com>
2020-03-12 00:13:39 +0900

    ディレクトリの構成変更

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.scss';
+import './assets/styles/style.scss';
 
 import App from './js/App';
 
